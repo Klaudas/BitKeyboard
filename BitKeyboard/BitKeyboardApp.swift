b282ec66a0ba36384d80726458882e282bbf93b6
klaudas <klaudasj@gmail.com>
2026-04-03 10:49:06 +0700

    Initial Commit

---
+++ BitKeyboard/BitKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitKeyboardApp.swift
+//  BitKeyboard
+//
+//  Created by klaudas on 03/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct BitKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
